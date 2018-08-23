c802c6c3ba7cbf430e69e304a31d0ca04601c696
yash upadhyay <yashup1997@gmail.com>
2018-08-23 22:46:48 +0530

    genetic algorithm

--- GeneticAlgorithm/GeneticAlgorithm.java
+++ GeneticAlgorithm/GeneticAlgorithm.java
@@ -1,5 +1,48 @@
 package geneticAlgorithm;
-
+import java.util.*;
 public class GeneticAlgorithm {
+
+  private Random randomGenerator;
+
+  public GeneticAlgorithm(){
+
+    this.randomGenerator = new Random();
+
+  }
+public Population evolvePopulation(Population population){
+
+  for(int i=0;i<population.size();i++){
+
+    Individual firstIndividual = randomSelection(population);
+    Individual secondIndividual = randomSelection(population);
+    Individual newIndividual= crossover(firstIndividual, secondIndividual);
+
+    newPopulation.saveIndividual(1, newIndividual);
+  }
+
+  for(int i = 0;i<newPopulation.size();++i){
+
+    mutate(newPopulation.getIndividual(i));
+
+  }
+
+  return newPopulation;
+}
+
+
+
+
+
+  private Individual randomSelection(Population population){
+    Population newPopulation = new Population(Constants.TOURNAMENT_SIZE);
+    for(int i =0;i<Constants.TOURNAMENT_SIZE;i++){
+
+      int randomIndex = (int) (Math.random() * population.size());
+      newPopulation.saveIndividual(1, population.getIndividual(randomIndex));
+    }
+    Individual fittestIndividual = newPopulation.getFittestIndividual();
+    return fittestIndividual;
+
+  }
 
 }
