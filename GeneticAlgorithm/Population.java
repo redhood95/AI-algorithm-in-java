2d97a933a82c77e85b746892c630c1a34a0b131d
yash upadhyay <yashup1997@gmail.com>
2018-08-23 20:31:30 +0530

    population initializer added in Population.java

--- GeneticAlgorithm/Population.java
+++ GeneticAlgorithm/Population.java
@@ -2,4 +2,26 @@ package geneticAlgorithm;
 
 public class Population {
 
+  private Individual[] Individuals;
+  public Population(int populationSize){
+
+    Individuals = new Individual(populationSize);
+
+  }
+
+  public void initializer(){
+
+    for(int i=0;i<Individual.length;i++){
+      Individual newIndividual = new Individual();
+      newIndividual.generateIndividual();
+      saveIndividual(i,newIndividual);
+
+    }
+  }
+
+
+    public Individual getIndividual(int index){
+
+      return this.Individual[index];
+    }
 }
