6e302ce90ef40bd152cf61b24257b2bfba2825fc
yash upadhyay <yashup1997@gmail.com>
2018-08-28 20:15:52 +0530

    app.java

--- GeneticAlgorithm/App.java
+++ GeneticAlgorithm/App.java
@@ -3,7 +3,24 @@ package geneticAlgorithm;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+
+
+        GeneticAlgorithm algorithm = new GeneticAlgorithm();
+        Population population = new Population(100);
+        population.initialize();
+
+        int generationCount = 0;
+
+        while (population.getFittest().getFitness() != Constants.MAX_FITNESS) {
+            ++generationCount;
+            System.out.println("Generation: " + generationCount + " Fittest: " + population.getFittest().getFitness());
+            System.out.println(population.getFittest()+"\n");
+            population = algorithm.evolvePopulation(population);
+        }
+
+        System.out.println("Solution found:");
+        System.out.println(population.getFittest());
 
 	}
 
