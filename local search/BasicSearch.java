4a2f6bd30cfaa1d69b4a1edccd8a91f02f4d3b32
yash upadhyay <yashup1997@gmail.com>
2018-08-29 20:17:19 +0530

    file edited

--- local search/BasicSearch.java
+++ local search/BasicSearch.java
@@ -1,4 +1,3 @@
-package com.balazsholczer.max;
 
 
 
@@ -7,19 +6,19 @@ public class BasicSearch {
 	public static double f(double x){
 		return -(x*x);
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		double startPointX = -2;
 		double max = f( startPointX );
-		
+
 		for(double i = startPointX ; i < 2 ; i+=0.1){
-			
+
 			if( f(i) > max ){
 				max = f(i);
 			}
 		}
-		
+
 		System.out.println("Maximum is: "+max);
 	}
 }
