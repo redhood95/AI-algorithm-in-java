491270b11e4db98a2f8bae693b0a984ead77200f
yash upadhyay <yashup1997@gmail.com>
2018-08-29 21:30:41 +0530

    simulated annealing

---
+++ SimulatedAnnealing/SimulatedAnnealing.java
@@ -0,0 +1,12 @@
+
+import java.util.Random;
+
+public class SimulatedAnnealing {
+
+	private Random randomGenerator;
+	private double currentCoordinateX;
+	private double nextCoordinateX;
+	private double bestCoordinateX;
+
+	
+}
