5195dac51c286db579c9bc0baf5939035f7cd4a2
yash upadhyay <yashup1997@gmail.com>
2018-08-30 22:48:17 +0530

    comments added

--- GraphAlgorithms/breadthFirstSearch/App.java
+++ GraphAlgorithms/breadthFirstSearch/App.java
@@ -1,18 +1,21 @@
 package breadthFirstSearch;
 
+/* purpose of this class is to work as the main appilcation program to run the algorithm*/
+
+
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		BFS bfs = new BFS();
-		
-		
+
+
 		Vertex vertex1 = new Vertex(1);
 		Vertex vertex2 = new Vertex(2);
 		Vertex vertex3 = new Vertex(3);
 		Vertex vertex4 = new Vertex(4);
 		Vertex vertex5 = new Vertex(5);
-		
+
 		vertex1.addNeighbourVertex(vertex2);
 		vertex1.addNeighbourVertex(vertex4);
 		vertex4.addNeighbourVertex(vertex5);
